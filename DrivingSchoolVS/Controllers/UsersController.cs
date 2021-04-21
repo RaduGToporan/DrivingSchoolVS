36cee191dbbb61dccc75904845423e01ee2aab0d
RaduGToporan <radutoporan@yahoo.com>
2021-04-21 22:59:56 +0300

    instructors improvements + Users CRUD

--- DrivingSchoolVS/Controllers/UsersController.cs
+++ DrivingSchoolVS/Controllers/UsersController.cs
@@ -5,30 +5,26 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using DrivingSchoolVS.Models;
 using DrivingSchool.Models;
 
 namespace DrivingSchoolVS.Controllers
 {
-    public class InstructorsController : Controller
+    public class UsersController : Controller
     {
         private readonly DrivingSchoolContext _context;
 
-        public InstructorsController(DrivingSchoolContext context)
+        public UsersController(DrivingSchoolContext context)
         {
             _context = context;
         }
 
-        // GET: Instructors
-        public async Task<IActionResult> Index(int page = 1)
+        // GET: Users
+        public async Task<IActionResult> Index()
         {
-            var qry = _context.Instructors.AsNoTracking().OrderBy(p => p.InstructorId);
-            int pageSize = 2;
-            var model = await PaginatedList<Instructor>.CreateAsync(qry, page, pageSize);
-            return View(model);
+            return View(await _context.Users.ToListAsync());
         }
 
-        // GET: Instructors/Details/5
+        // GET: Users/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -36,40 +32,39 @@ namespace DrivingSchoolVS.Controllers
                 return NotFound();
             }
 
-            var instructor = await _context.Instructors.Include(instructor => instructor.Vehicles)
-                .ThenInclude(vehicle => vehicle.DrivingCategory)
-                .FirstOrDefaultAsync(m => m.InstructorId == id);
-            if (instructor == null)
+            var user = await _context.Users
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (user == null)
             {
                 return NotFound();
             }
 
-            return View(instructor);
+            return View(user);
         }
 
-        // GET: Instructors/Create
+        // GET: Users/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Instructors/Create
+        // POST: Users/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("InstructorId,Name,YearOfBirth,PhoneNumber,Email")] Instructor instructor)
+        public async Task<IActionResult> Create([Bind("UserId,Name")] User user)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(instructor);
+                _context.Add(user);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(instructor);
+            return View(user);
         }
 
-        // GET: Instructors/Edit/5
+        // GET: Users/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -77,33 +72,22 @@ namespace DrivingSchoolVS.Controllers
                 return NotFound();
             }
 
-            var instructor = await _context.Instructors.FindAsync(id);
-            if (instructor == null)
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
             {
                 return NotFound();
             }
-            return View(instructor);
+            return View(user);
         }
 
-        public IActionResult AddVehicles(int instructorId, Vehicle vehicle)
-        {
-            Instructor instr = _context.Instructors.Find(instructorId);
-            _context.Vehicles.Add(vehicle);
-            _context.SaveChanges();
-            instr.Vehicles.Add(vehicle);
-            _context.Instructors.Update(instr);
-            _context.SaveChanges();
-            return RedirectToAction("Details", new { id=instructorId });
-        }
-
-        // POST: Instructors/Edit/5
+        // POST: Users/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("InstructorId,Name,YearOfBirth,PhoneNumber,Email")] Instructor instructor)
+        public async Task<IActionResult> Edit(int id, [Bind("UserId,Name")] User user)
         {
-            if (id != instructor.InstructorId)
+            if (id != user.UserId)
             {
                 return NotFound();
             }
@@ -112,12 +96,12 @@ namespace DrivingSchoolVS.Controllers
             {
                 try
                 {
-                    _context.Update(instructor);
+                    _context.Update(user);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!InstructorExists(instructor.InstructorId))
+                    if (!UserExists(user.UserId))
                     {
                         return NotFound();
                     }
@@ -128,10 +112,10 @@ namespace DrivingSchoolVS.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(instructor);
+            return View(user);
         }
 
-        // GET: Instructors/Delete/5
+        // GET: Users/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -139,30 +123,30 @@ namespace DrivingSchoolVS.Controllers
                 return NotFound();
             }
 
-            var instructor = await _context.Instructors
-                .FirstOrDefaultAsync(m => m.InstructorId == id);
-            if (instructor == null)
+            var user = await _context.Users
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (user == null)
             {
                 return NotFound();
             }
 
-            return View(instructor);
+            return View(user);
         }
 
-        // POST: Instructors/Delete/5
+        // POST: Users/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var instructor = await _context.Instructors.FindAsync(id);
-            _context.Instructors.Remove(instructor);
+            var user = await _context.Users.FindAsync(id);
+            _context.Users.Remove(user);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool InstructorExists(int id)
+        private bool UserExists(int id)
         {
-            return _context.Instructors.Any(e => e.InstructorId == id);
+            return _context.Users.Any(e => e.UserId == id);
         }
     }
 }
